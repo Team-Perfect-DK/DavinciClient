ecbd3f0d4b711fffdf9b6b37b9c294a5af91a5e0
Jiwon Park <wldnjs1123@gmail.com>
2025-02-04 23:07:54 +0900

    [chore] websocket 주소 변경

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -14,7 +14,7 @@ const Home = () => {
 
   useEffect(() => {
     const wsProtocol = window.location.protocol === "https:" ? "wss" : "ws";
-    const ws = new WebSocket(`${wsProtocol}://davinci-code.net/ws`);
+    const ws = new WebSocket(`${wsProtocol}://api.davinci-code.net/ws`);
 
 
     ws.onopen = () => {
